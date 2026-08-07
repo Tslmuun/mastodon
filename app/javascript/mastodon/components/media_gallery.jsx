4e526ca9f347f91f78f4e0a04e9770d5332b8143
Tslmuun <50679343+Tslmuun@users.noreply.github.com>
2026-08-07 18:29:00 +0800

    Add 3-column media layouts and span-3
    Enable additional media-gallery layouts (5–9) and support items spanning three columns. Updated media_gallery.jsx to include spans-aware classnames (treat spans===2 as wide, add media-gallery__item--span-3 for spans===3). Updated styles: added layout-5..layout-9 to mastodon-bird-ui _media.scss and added 3-column grid rules plus &--span-3 in mastodon/components.scss. This allows 3-column gallery displays and items that span all three columns.

--- app/javascript/mastodon/components/media_gallery.jsx
+++ app/javascript/mastodon/components/media_gallery.jsx
@@ -131,7 +131,7 @@ class Item extends PureComponent {
 
     if (attachment.get('type') === 'unknown') {
       return (
-        <div className={classNames('media-gallery__item', { standalone, 'media-gallery__item--tall': height === 100, 'media-gallery__item--wide': width === 100 })} key={attachment.get('id')}>
+        <div className={classNames('media-gallery__item', { standalone, 'media-gallery__item--tall': height === 100, 'media-gallery__item--wide': width === 100 || spans === 2, 'media-gallery__item--span-3': spans === 3 })} key={attachment.get('id')}>
           <a className='media-gallery__item-thumbnail' href={attachment.get('remote_url') || attachment.get('url')} style={{ cursor: 'pointer' }} title={description} lang={lang} target='_blank' rel='noopener'>
             <Blurhash
               hash={attachment.get('blurhash')}
@@ -210,7 +210,7 @@ class Item extends PureComponent {
     }
 
     return (
-      <div className={classNames('media-gallery__item', { standalone, 'media-gallery__item--error': this.state.error, 'media-gallery__item--tall': height === 100, 'media-gallery__item--wide': width === 100 })} key={attachment.get('id')}>
+      <div className={classNames('media-gallery__item', { standalone, 'media-gallery__item--error': this.state.error, 'media-gallery__item--tall': height === 100, 'media-gallery__item--wide': width === 100 || spans === 2, 'media-gallery__item--span-3': spans === 3 })} key={attachment.get('id')}>
         <Blurhash
           hash={attachment.get('blurhash')}
           dummy={!useBlurhash}
